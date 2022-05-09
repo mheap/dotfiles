5d7508ae2de96cf35aa9f6d3479c62bdc7449507
Michael Heap <m@michaelheap.com>
2022-05-09 10:12:30 +0100

    Fix install script for codespaces

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/usr/bin/zsh
+
+if [[ -z "$ZSH_NAME" ]]; then
+  zsh ${0}
+  exit
+fi
 
 set -e # -e: exit on error
 
+STARSHIP_FILE=/workspaces/.codespaces/.persistedshare/dotfiles/starship.sh
+if [ ! -f $STARSHIP_FILE ] ; then
+  curl https://starship.rs/install.sh > $STARSHIP_FILE
+  sh $STARSHIP_FILE -y
+fi
+
 if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/.local/bin"
+  mkdir "$HOME/.local/share/chezmoi"
   chezmoi="$bin_dir/chezmoi"
   if [ "$(command -v curl)" ]; then
     sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
@@ -17,13 +29,25 @@ else
   chezmoi=chezmoi
 fi
 
-# Install zplug
-export ZPLUG_HOME="/workspaces/.zplug"
-git clone https://github.com/zplug/zplug $ZPLUG_HOME
-zplug install
-
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-# exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir"
+"$chezmoi" init --apply "--source=$script_dir"
 
+# Install required packages
+sudo apt-get install -y lua5.3 > /dev/null
+
+# Install zplug
+export ZPLUG_HOME="/workspaces/.zplug"
+if [ ! -d $ZPLUG_HOME ] ; then
+  git clone https://github.com/zplug/zplug $ZPLUG_HOME
+
+  source $ZPLUG_HOME/init.zsh
+  # Install plugins if there are plugins that have not been installed
+  if ! zplug check --verbose; then
+    zplug install
+  fi
+fi
+
+# Fix compinit errors
+compaudit | xargs chmod g-w
+compaudit | xargs chmod a-w
