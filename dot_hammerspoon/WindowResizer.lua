048cb3c2306f1e9abd0054459139060973b62364
Michael Heap <m@michaelheap.com>
2022-10-02 11:04:47 +0100

    More Hammerspoon screen logic

--- dot_hammerspoon/WindowResizer.lua
+++ dot_hammerspoon/WindowResizer.lua
@@ -1,19 +1,25 @@
 
 -- Split screen - X windows
-function setPercentScreen(bind, portion, key, index)
+function setPercentScreen(bind, portion, key, index, altScreen)
   hs.hotkey.bind(bind, key, function()
     local win = hs.window.focusedWindow()
 
     local section_width = (5120/portion) -- 5120 is the screen width
+    local xmod = 0;
+    if altScreen then
+      section_width = (3440/portion) -- 3440 is the screen width for the alt screen
+      xmod = -3440
+    end
   
     local cursize = win:size()
     cursize.w = section_width
     cursize.h = 1440
     
     local f = win:frame()
-    f.x = index * section_width
+    f.x = (index * section_width)+xmod
     f.y = 0
   
+
     win:setFrame(f)
     win:setSize(cursize)
   end)
@@ -54,3 +60,7 @@ setPercentScreen({"cmd", "alt", "ctrl"}, 8, "N", 4)
 setPercentScreen({"cmd", "alt", "ctrl"}, 8, "M", 5)
 setPercentScreen({"cmd", "alt", "ctrl"}, 8, ",", 6)
 setPercentScreen({"cmd", "alt", "ctrl"}, 8, ".", 7)
+
+setPercentScreen({"cmd", "alt", "shift"}, 1, ".", 0, true)
+setPercentScreen({"cmd", "alt", "shift"}, 2, "N", 0, true)
+setPercentScreen({"cmd", "alt", "shift"}, 2, "M", 1, true)
